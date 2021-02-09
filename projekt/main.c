c40624810c323a501e424c4715df507e5a1ef30b
geogarden <geo.garden@outlook.com>
2021-02-09 08:29:19 +0100

    pierwsze push

---
+++ projekt/main.c
@@ -0,0 +1,39 @@
+/*
+ * main.c
+ *
+ *  Created on: 8 lut 2021
+ *      Author: geoga
+ */
+// podstawa programów demonstracyjnych //
+// wykorzystujących bezwładnośc wzroku //
+// ------- Preambu│a -------- //
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+// -------- Funkcje --------- //
+void POVDisplay( uint8_t oneByte ) {
+    PORTB = oneByte;
+    _delay_ms( 2 );
+}
+int main( void ) {
+// -------- Inicjacja --------- //
+    DDRB = 0xff; /* Wszystkie piny z diodami LED ustaw jako wyjscia */
+// ------ petla zdarzen ------ //
+    while ( 1 ) { /* pŕtla g│ˇwna */
+        POVDisplay( 0b00001110 );
+        POVDisplay( 0b00011000 );
+        POVDisplay( 0b10111101 );
+        POVDisplay( 0b01110110 );
+        POVDisplay( 0b00111100 );
+        POVDisplay( 0b00111100 );
+        POVDisplay( 0b00111100 );
+        POVDisplay( 0b01110110 );
+        POVDisplay( 0b10111101 );
+        POVDisplay( 0b00011000 );
+        POVDisplay( 0b00001110 );
+        PORTB = 0;
+        _delay_ms( 10 );
+    } /* koniec pętli głównej */
+    return ( 0 );
+}
